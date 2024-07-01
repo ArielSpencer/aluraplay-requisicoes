7575adbedd295d6affdd36ea3285032cd0829384
Ariel Spencer <arielspencer.tech@gmail.com>
2024-07-01 03:15:56 -0300

    Mostrar mensagem de erro ao carregar videos

--- js/mostrarVideos.js
+++ js/mostrarVideos.js
@@ -19,9 +19,14 @@ export default function constroiCard(titulo, descricao, url, imagem) {
 }
 
 async function listaVideos() {
+    try {
     const listaApi = await conectaApi.listaVideos();
     listaApi.forEach(elemento => lista.appendChild(
         constroiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem)))
+} catch {
+    lista.innerHTML = `<img src="./img/erro-carregamento.svg" class="mensagem__image-erro">
+    <h2 class="mensagem__titulo">Não foi possível carregar a lista de vídeos!</h2>`
+}
 }
 
 listaVideos();
